a2c501bcd0c27d18bbdfb503e07bf5a0412d410d
Shawon Ashraf <shawon13@live.com>
2018-05-11 15:24:25 +0600

    imported config json in config.js

---
+++ server/config/config.js
@@ -0,0 +1,13 @@
+var configJSON = require('./config.json');
+
+var PUSHER_APP_ID = configJSON[PUSHER_APP_ID];
+var PUSHER_APP_KEY = configJSON[PUSHER_APP_KEY];
+var PUSHER_APP_SECRET = configJSON[PUSHER_APP_SECRET];
+var PUSHER_APP_CLUSTER = configJSON[PUSHER_APP_CLUSTER];
+
+module.exports = {
+    PUSHER_APP_ID,
+    PUSHER_APP_KEY,
+    PUSHER_APP_SECRET,
+    PUSHER_APP_CLUSTER
+};
