ece6e665f995a81b13b83cd3d4c7cd8664d9291d
Shawon Ashraf <shawon13@live.com>
2018-05-11 15:32:37 +0600

    fixed import in config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,13 @@
 var configJSON = require('./config.json');
 
-var PUSHER_APP_ID = configJSON[PUSHER_APP_ID];
-var PUSHER_APP_KEY = configJSON[PUSHER_APP_KEY];
-var PUSHER_APP_SECRET = configJSON[PUSHER_APP_SECRET];
-var PUSHER_APP_CLUSTER = configJSON[PUSHER_APP_CLUSTER];
+var appId = configJSON['PUSHER_APP_ID'].toString();
+var key = configJSON['PUSHER_APP_KEY'].toString();
+var secret = configJSON['PUSHER_APP_SECRET'].toString();
+var cluster = configJSON['PUSHER_APP_CLUSTER'].toString();
 
 module.exports = {
-    appId: PUSHER_APP_ID,
-    key: PUSHER_APP_KEY,
-    secret: PUSHER_APP_SECRET,
-    cluster: PUSHER_APP_CLUSTER
+    appId,
+    key,
+    secret,
+    cluster
 };
