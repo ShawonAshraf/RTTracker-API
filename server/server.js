e4536ae67d100e1defdea15fb69b186c7037b507
Shawon Ashraf <shawon13@live.com>
2018-05-11 14:38:32 +0600

    pusher init

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 // dependencies
 const express = require('express');
-const pusher = require('pusher');
+const Pusher = require('pusher');
 const bodyparser = require('body-parser');
 
 
 // init
 var app = express();
+var pusher = new Pusher(require('./config/config'));
 const port = process.env.PORT || 3000;
 
 // register middlewares
