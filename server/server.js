09f56ddb13b698a51db53f259516be70dc409253
Shawon Ashraf <shawon13@live.com>
2018-05-11 14:37:22 +0600

    middleware reg: server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,14 @@ const express = require('express');
 const pusher = require('pusher');
 const bodyparser = require('body-parser');
 
+
+// init
 var app = express();
 const port = process.env.PORT || 3000;
+
+// register middlewares
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
 
 app.listen(port, () => {
     console.log('Server running @ PORT ', port);
