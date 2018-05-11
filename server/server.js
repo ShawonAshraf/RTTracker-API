fb7d137ce4c23e68d3b6889e1a278f332c7e4848
Shawon Ashraf <shawon13@live.com>
2018-05-11 14:56:20 +0600

    api/simloc route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,33 @@ const port = process.env.PORT || 3000;
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
+// routes
+//
+//  POST simloc
+// simulates 20 GPS Coordinates and sends to pusher
+
+app.post('/api/simloc', (req, res) => {
+    var loopCount = 0;
+    var operator = 0.001000;
+    var longitude = parseFloat(req.body.longitude);
+    var lattitude = parseFloat(req.body.lattitude);
+
+    var sendToPusher = setInterval(() => {
+        loopCount++;
+
+        longitude = parseFloat((longitude + operator).toFixed(7));
+        lattitude = parseFloat((lattitude - operator).toFixed(7));
+
+        pusher.trigger('mapCoordinates', 'update', { longitude, lattitude });
+
+        if (loopCount === 20) {
+            clearInterval(sendToPusher);
+        }
+    }, 2000);
+
+    res.status(200).send({ message: 'success', code: 200 });
+});
+
 app.listen(port, () => {
     console.log('Server running @ PORT ', port);
 });
